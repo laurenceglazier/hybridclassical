a4de9913138b1e933b0d6193fc5bfea9404080df
Laurence Glazier <laurence@laurenceglazier.com>
2023-02-19 00:31:14 +0000

    add toggle buttons to enable tempo and volume change by tilt

--- js/app.js
+++ js/app.js
@@ -171,3 +171,11 @@ function detectShake(event) {
     setVolume(currentVolume + volumeChange);
   }
 }
+
+function toggleButton(button) {
+  if (button.classList.contains("pressed")) {
+    button.classList.remove("pressed");
+  } else {
+    button.classList.add("pressed");
+  }
+}
