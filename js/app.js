d97efd4e161306de52b81d9dd39cbc55b2495f8d
Laurence Glazier <laurence@laurenceglazier.com>
2023-02-19 18:50:18 +0000

    remove older timing code for shake gesture, new file control-volume.js

--- js/app.js
+++ js/app.js
@@ -5,13 +5,6 @@ let xValue = 0;
 let yValue = 0;
 let zValue = 0;
 
-// ... and display them
-document.getElementById("xValue").innerHTML = "X = " + xValue.toFixed(2);
-document.getElementById("yValue").innerHTML = "Y = " + yValue.toFixed(2);
-document.getElementById("zValue").innerHTML = "Z = " + zValue.toFixed(2);
-document.getElementById("motion-sensor-value").innerHTML =
-  "motion-sensor-value = " + yValue.toFixed(2);
-
 // Get the volume slider element
 const volumeSlider = document.getElementById("volumeSlider");
 volumeSlider.addEventListener("touchstart", function (event) {
@@ -41,90 +34,17 @@ window.addEventListener("devicemotion", function (event) {
     document.getElementById("motion-sensor-value").innerHTML =
       "Y: " +
       y.toFixed(2) +
-      " prevY: " +
-      prevY.toFixed(2) +
       " Vol: " +
       currentVolume +
       " Slider: " +
       volumeSlider.value;
-    // alert("Passed first if - Y: "+ y +" prevY: " + prevY.toFixed(2));
 
-    /*
-    if (Date.now() - prevTime <= 1000) {
-        // alert("Threshold");
-        // Update the previous y value and time
-        prevY = y;
-        prevTime = Date.now();
-
-        // Increase the volume if it is less than 1
-        if (currentVolume < 1) {
-        currentVolume += 0.1;
-        }
-        // Set the volume of the audio element
-        audioElement.volume = currentVolume;
-        // Update the volume slider value
-        volumeSlider.value = currentVolume;
-    }
-    */
-
-    // Increase the volume if it is less than 1
-    if (Date.now() - prevTime <= 10000000) {
-      // alert(currentVolume);
-      if (currentVolume <= 1.5) {
-        // currentVolume += 0.1;
-        //currentVolume=currentVolume+.1
-        //alert(currentVolume);
-        // Increment the volume value
-        let currentValue = Number(volumeSlider.value);
-        //let currentVolume = Number(volumeSlider.value);
-        if (y > prevY) {
-          volumeSlider.value = Math.min(currentValue + 0.1, 1);
-          currentVolume = Math.min(currentVolume + 0.1, 1);
-          document.getElementById("motion-sensor-value").innerHTML =
-            "Y: " +
-            y.toFixed(2) +
-            " prevY: " +
-            prevY.toFixed(2) +
-            " Vol: " +
-            currentVolume +
-            " Slider: " +
-            volumeSlider.value +
-            " UP";
-        }
-        if (y < prevY) {
-          volumeSlider.value = Math.min(currentValue - 0.1, 1);
-          currentVolume = Math.min(currentVolume - 0.1, 1);
-          document.getElementById("motion-sensor-value").innerHTML =
-            "Y: " +
-            y.toFixed(2) +
-            " prevY: " +
-            prevY.toFixed(2) +
-            " Vol: " +
-            currentVolume +
-            " Slider: " +
-            volumeSlider.value +
-            " DOWN";
-        }
-
-        //if (y<prevY) {
-        //  volumeSlider.value = Math.min(currentValue - 0.01, 1);
-        //}
-        setVolume(volumeSlider.value);
-      }
-    }
-
-    prevY = y;
-
-    // Set the volume of the audio element
-    // audioElement.volume = currentVolume;
-    // Update the volume slider value
     volumeSlider.value = currentVolume;
   }
 });
 
 // Add event listener for keydown event
 document.addEventListener("keydown", function (event) {
-
   if (event.code === "ArrowUp") {
     // Increment the volume value
     let currentValue = Number(volumeSlider.value);
@@ -153,23 +73,6 @@ function setVolume(value) {
   audioElement.volume = value;
   currentVolume = value;
   // document.getElementById("volume-value").innerHTML = value;
-}
-
-
-function detectShake(event) {
-  const { acceleration } = event;
-  if (!acceleration) {
-    return;
-  }
-
-  const yAcceleration = acceleration.y;
-
-  if (Math.abs(yAcceleration) > shakeThreshold) {
-    // Change the volume based on the yAcceleration value
-    // For example:
-    const volumeChange = Math.sign(yAcceleration) * volumeStep;
-    setVolume(currentVolume + volumeChange);
-  }
 }
 
 function toggleButton(button) {
